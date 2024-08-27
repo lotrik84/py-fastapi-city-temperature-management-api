86dcc87a34e0259b6201e58d1baf1a110990b4f3
Oleksandr Pastukhov <lotrikys@gmail.com>
2024-08-27 16:37:20 +0300

    * Add temperature model and db migration

--- db/models.py
+++ db/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, Integer, String
+import datetime
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
+from sqlalchemy.orm import relationship
 
 from db.engine import Base
 
@@ -8,3 +11,12 @@ class DBCity(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), nullable=False)
     additional_info = Column(String(1000), nullable=True)
+
+
+class DBTemperature(Base):
+    __tablename__ = "temperature"
+    id = Column(Integer, primary_key=True, index=True)
+    city_id = Column(Integer, ForeignKey("city.id"), nullable=False)
+    city = relationship(DBCity)
+    date_time = Column(DateTime, default=datetime.datetime.now)
+    temperature = Column(Float, nullable=False)
